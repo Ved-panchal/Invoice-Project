12491a9a286d16cc19b50c291c7424e65a9faa85
Mitul <mitulnagani8@gmail.com>
2024-09-23 14:18:49 +0530

    Created endpoint for only JSON data

--- backend/routes/api_route.py
+++ backend/routes/api_route.py
@@ -1,5 +1,6 @@
 from fastapi import File, HTTPException, UploadFile, BackgroundTasks, APIRouter, Depends
-from starlette.responses import Response
+from starlette.responses import Response, JSONResponse
+
 
 import os, json, requests
 from datetime import datetime
@@ -9,10 +10,12 @@ from io import BytesIO
 
 from models import PDFApprovalStatus, PDFUploadStatus
 from util import utils, pdf_utils
-from extraction import get_pdf_name
+from extraction import get_pdf_name, store_pdf_data
 from database import mongo_conn
 from queue_service import queue_manager
 from routes import login_manager, STATIC_DIR
+from together import Together
+from decouple import config
 
 api_router = APIRouter()
 
@@ -207,7 +210,7 @@ def get_fields(response: Response, user=Depends(login_manager)):
     except Exception as e:
         response.status_code = 500
         raise HTTPException(status_code=500, detail=f"Unknown error has occured.\nDetails={str(e)}")
-    
+
 @api_router.post('/update_fields')
 def update_fields(response: Response, payload: dict, user=Depends(login_manager)):
     try:
@@ -230,14 +233,14 @@ def update_fields(response: Response, payload: dict, user=Depends(login_manager)
         response.status_code = 500
         response.body = json.dumps({"message": f"Unknown error has occured.\nDetails={str(e)}"}).encode()
         raise HTTPException(status_code=500, detail=f"Unknown error has occured.\nDetails={str(e)}")
-    
+
 
 def sap_login_call(sap_details):
-    print("making login request")
+    # print("making login request")
     response = requests.post(f"https://{sap_details['sapHost']}/b1s/v1/Login", json=sap_details['loginCredential'], verify=False)
-    print("login request status", response.status_code)
+    # print("login request status", response.status_code)
     data = json.loads(response.content.decode('utf-8'))
-    print("login request data", data)
+    # print("login request data", data)
     if response.status_code == 200:
         return data['SessionId']
     else:
@@ -261,10 +264,10 @@ def make_sap_call(sap_details, cardName):
             return {"status": "failed login"}
         elif response.status_code == 200:
             data = json.loads(response.content.decode('utf-8'))
-            print('data by sap api', data)
+            # print('data by sap api', data)
             return {"status": "ok", "data": data, "sessionId": sap_details['sessionId']}
         else:
-            print('unknown error in sap : ', sap_details)
+            # print('unknown error in sap : ', sap_details)
             return {"status": "failed"}
 
 
@@ -272,9 +275,9 @@ def make_sap_call(sap_details, cardName):
 def get_sap_data(payload: dict, user=Depends(login_manager)):
     try:
         user_id = user['userId']
-        print("payload ->",payload)
+        # print("payload ->",payload)
         sap_details = mongo_conn.get_sap_detalis_collection().find_one({"userId" : user_id}, {"_id" : 0})
-        print("user id : ", user_id)
+        # print("user id : ", user_id)
         if sap_details is not None:
             data = make_sap_call(sap_details, payload['cardName'])
             if data['sessionId'] != sap_details['sessionId']:
@@ -292,3 +295,91 @@ def get_sap_data(payload: dict, user=Depends(login_manager)):
             return {"status": "failed", "message":"Your Sap is not configured"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Unknown error has occured.\nDetails={str(e)}")
+    
+
+@api_router.post("/get_json_data/{user_id}")
+async def upload_files_json(user_id: str, response: Response, background_tasks: BackgroundTasks, documents: list[UploadFile] = File(...), user=Depends(login_manager)):
+    filenames, uploaded_arr, not_uploaded_arr = [], [], []
+    try:
+        user_id = user['userId']
+        user_dir = STATIC_DIR / user_id
+        if not user_dir.exists():
+            os.makedirs(user_dir)
+
+        # Get total credits from db
+        credits = mongo_conn.get_users_collection().find_one({"userId": user_id}, {"totalCredits": 1, "_id": 0})
+        total_credits = credits['totalCredits']
+        # print(f"total credits: {total_credits}")
+
+        for document in documents:
+            file_ext = document.filename.split('.')[-1].lower()
+
+            file_data = document.file.read()
+            buf = BytesIO(file_data)
+
+            if file_ext == 'pdf':
+                total_pages = pdf_utils.get_total_pages_pdf(buf)
+            elif file_ext == 'docx':
+                total_pages = pdf_utils.get_docx_page_count(buf)
+            else:
+                total_pages = 0
+                raise Exception("Invalid file type.")
+
+            # # get total pages for current document
+            # print(f"total pages: {total_pages}")
+
+            # check if total pages < total credits if yes then allow and subtract total credits by total pages else continue
+            if total_pages <= total_credits:
+                total_credits -= total_pages
+
+                pdf_data = {
+                    "userId": user_id,
+                    "pdfData": {
+                        "pdfId":"",
+                        "pdfName": document.filename,
+                        "pdfStatus": PDFUploadStatus.PENDING,
+                        "pdfApprovalStatus": PDFApprovalStatus.PENDING,
+                        "createdAt": datetime.now(),
+                    }
+                }
+
+                result = mongo_conn.get_user_pdf_mapping_collection().insert_one(pdf_data)
+                pdf_data['_id'] = str(result.inserted_id) if result is not None else ""
+                pdf_data['pdfData']['id'] = pdf_data['_id']
+                new_file_name = f'{pdf_data["_id"]}.{file_ext}'
+                filenames.append(new_file_name)
+                file_location = user_dir / new_file_name
+
+                with open(file_location, "wb") as f:
+                    f.write(file_data)
+
+                uploaded_arr.append(document.filename)
+
+            else:
+                not_uploaded_arr.append(document.filename)
+
+
+        # Schedule the upload_files_to_queue task as a background task
+
+        client = Together(
+            api_key=config(f"TOGETHER_API_KEY_4")
+        )
+        resultArr = []
+        for Index ,file in enumerate(filenames):
+            # print(file)
+            result = store_pdf_data(client,user_id,file,STATIC_DIR)
+            # print(result)
+            if result is not None:
+                response_json = {"FileName" : uploaded_arr[Index], "Data": result[2]}
+            else:
+                response_json = {"FileName" : uploaded_arr[Index], "Error": "Cant find result, Look into UI for status of pdf"}
+            resultArr.append(response_json)
+        response.status_code = 200
+        response.body = json.dumps({"Uploaded Files" : resultArr}).encode()
+
+    except Exception as e:
+        print("Error uploading files:", e)
+        response.status_code = 500
+        response.body = json.dumps({"error": str(e)}).encode()
+
+    return response
